7efb9ba72d269bc8abbce0de63c51f6676e8a3c8
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-03 15:42:10 +0530

    porcfile created push to heroku

--- server/server.js
+++ server/server.js
@@ -18,3 +18,14 @@ http.createServer(app)
       console.log(`up and running @: ${os.hostname()} on port: ${PORT}`);
       console.log(`enviroment: ${process.env.NODE_ENV}`);
     });
+
+
+/*
+ express =require('express'),
+  path=require('path');
+const  app =express();
+app.use(express.static('./dist/sample-PEAN'))
+app.get('/!*', (req,res)=>{
+  res.sendFile(path.json(__dirname,'/dist/sample-PEAN/index.html'));})
+app.listen(process.env.PORT||8080,()=>{console.log('server started')})
+*/
