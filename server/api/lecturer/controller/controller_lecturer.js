c839cdccd1d0d8f3791f278025f0fca32ecdda65
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-03 16:34:46 +0530

    porcfile created push to heroku

--- server/api/lecturer/controller/controller_lecturer.js
+++ server/api/lecturer/controller/controller_lecturer.js
@@ -8,11 +8,11 @@ import LecturerDao from "/home/sb-saikumar/WebstormProjects/sample-PEAN/server/a
       .catch(error => res.status(404).json(error));
    }
 
-  static createNew(req,res){
+   static createNew(req,res){
     LecturerDao
       .createNew(req.body)
       .then(lecturer=>res.status(200).json(lecturer))
       .catch(error=>res.status(400).json(error))
-  }
+   }
 
 }
