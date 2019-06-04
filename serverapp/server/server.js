af77463ff3b37a5e9e71244de6ecb3508e0d145a
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-04 17:38:14 +0530

    server push to heroku

--- serverapp/server/server.js
+++ serverapp/server/server.js
@@ -1,40 +1,16 @@
-/*
 const PORT = process.env.PORT || 4444;
-import express from "express";
-import http from "http";
-import RoutesConfig from "./config/routes.conf";
-import DBConfig from "./config/db.conf";
-import Routes from "./routes";
-
-const app = express();
-app.get("/",(req,res)=>{
-    res.send("ok its work");
-})
-RoutesConfig.init(app);
-DBConfig.init();
-Routes.init(app, express.Router());
- http.createServer(app)
-    .listen(PORT, () => {
-      console.log(`up and running @: ${os.hostname()} on port: ${PORT}`);
-      console.log(`enviroment: ${process.env.NODE_ENV}`);
-    });
-*/
-
-
-const PORT = process.env.PORT || 3333;
-
 import os from "os";
 import express from "express";
 import http from "http";
 import RoutesConfig from "./config/routes.conf";
-import DBConfig from "./config/db.conf";
-import Routes from "./routes/index";
+ import Routes from "./routes/index";
 
 const app = express();
-
+app.get("/",(req,res)=>{
+  res.send("hai");
+})
 RoutesConfig.init(app);
-DBConfig.init();
-Routes.init(app, express.Router());
+ Routes.init(app, express.Router());
 
 http.createServer(app)
     .listen(PORT, () => {
